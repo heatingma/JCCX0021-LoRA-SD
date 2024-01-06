08e68af0956a2e94c66d3ae4756a3e5e5c632ba7
heatingma <115260102+heatingma@users.noreply.github.com>
2024-01-06 12:18:05 +0800

    Add outputs

--- scripts/generate_text2img.sh
+++ scripts/generate_text2img.sh
@@ -2,4 +2,4 @@ CUDA_VISIBLE_DEVICES=3 python text2img/generate.py \
   --prompt "liuyifei, nice, pretty, beauty, smooth, Highest quality, ultra-high definition, masterpieces" \
   --filename "liuyifei" \
   --samples_num 3 \
-  --lora_dir "text2img/lora-finetune/lyf30"
+  --lora_dir "text2img/lora-finetune/lyf"
